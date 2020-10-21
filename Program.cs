33c0a2c54717cec843c9a06245311821bdfb8e17
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 20:16:41 +0530

    [Kalyan] added funtionality finding for length of line

--- Program.cs
+++ Program.cs
@@ -6,9 +6,36 @@ namespace LineComaparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation");
-            
+            double temp1 = GetEndPointsOfX();
+            double temp2 = GetEndPointsOfY();
+            double result1 = DistanceBtwnPoints(temp1, temp2);
+            Console.WriteLine("Distance between two points: " + result1);
+
         }
+
+        public static double GetEndPointsOfX()
+        {
+            Console.WriteLine("Enter x1,x2");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var x2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Pow((x2 - x1), 2); 
+        }
+
+        public static double GetEndPointsOfY()
+        {
+            Console.WriteLine("Enter y1,y2");
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Pow((y2 - y1), 2); 
+        }
+        
+        public static double DistanceBtwnPoints(double temp1, double temp2)
+        {
+            return Math.Sqrt(temp1 + temp2);
+        }
+
     }
 }
 
