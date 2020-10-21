7fff66b5d4c5846d2ef5899b9d40262bd9ea9fba
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 19:59:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LineComaparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation");
+            
+        }
+    }
+}
+
+
+
